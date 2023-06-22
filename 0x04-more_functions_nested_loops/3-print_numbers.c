18539fd225026a4ecbfaf10ad5acfe38e3d4d26b
Maha EL OUARDI <maha600019@gmail.com>
2023-06-22 04:04:45 -0700

    hi

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,28 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
  */
-void print_numbers(void)
-{
-int i;
 
-i = 0;
-while (i < 10)
+void more_numbers(void)
 {
-_putchar (i + '0');
-i++;
-}
+	int number, tens, units, row;
 
-_putchar ('\n');
+	for (row = 1; row <= 10; row++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+	}
 }
