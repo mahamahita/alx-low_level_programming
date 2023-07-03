4b246f9813916c6b08a25c9fe4ef8acd2f8029fb
Maha EL OUARDI <maha600019@gmail.com>
2023-07-03 03:43:37 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+/**
+ * _memset - a function that fills
+ *         memory with a constant byte
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
