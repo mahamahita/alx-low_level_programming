22b5147ed9d5afe2d03625383eb9dea0db9ad9f8
MAHA EL OUARDI <maha600019@gmail.com>
2023-08-09 07:17:46 -0700

    yy

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,8 +1,3 @@
-/*
- * File: 100-elf_header.c
- * Auth: Brennan D Baraban
- */
-
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,20 +19,19 @@ void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
- * Description: If the file is not an ELF file - exit code 98.
+ * @e_ident:  pointer to the  array containing ELF magic numbers.
+ * return: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
 {
-	int index;
+	int i;
 
-	for (index = 0; index < 4; index++)
+	for (index = 0; i < 4; i++)
 	{
-		if (e_ident[index] != 127 &&
-		    e_ident[index] != 'E' &&
-		    e_ident[index] != 'L' &&
-		    e_ident[index] != 'F')
+		if (e_ident[i] != 127 &&
+		    e_ident[i] != 'E' &&
+		    e_ident[i] != 'L' &&
+		    e_ident[i] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -47,21 +41,21 @@ void check_elf(unsigned char *e_ident)
 
 /**
  * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: pointer to the  array containing ELF magic numbers.
  *
- * Description: Magic numbers are separated by spaces.
+ * return: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
 {
-	int index;
+	int i;
 
 	printf("  Magic:   ");
 
-	for (index = 0; index < EI_NIDENT; index++)
+	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x", e_ident[index]);
+		printf("%02x", e_ident[i]);
 
-		if (index == EI_NIDENT - 1)
+		if (i == EI_NIDENT - 1)
 			printf("\n");
 		else
 			printf(" ");
@@ -70,7 +64,7 @@ void print_magic(unsigned char *e_ident)
 
 /**
  * print_class - Prints the class of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @e_ident: pointer to the array containing ELF class.
  */
 void print_class(unsigned char *e_ident)
 {
@@ -94,7 +88,7 @@ void print_class(unsigned char *e_ident)
 
 /**
  * print_data - Prints the data of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @e_ident:  pointer to the array containing ELF class.
  */
 void print_data(unsigned char *e_ident)
 {
@@ -118,7 +112,7 @@ void print_data(unsigned char *e_ident)
 
 /**
  * print_version - Prints the version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
+ * @e_ident:  pointer to the array containing ELF version.
  */
 void print_version(unsigned char *e_ident)
 {
@@ -138,7 +132,7 @@ void print_version(unsigned char *e_ident)
 
 /**
  * print_osabi - Prints the OS/ABI of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
+ * @e_ident:  pointer to the array containing the version.
  */
 void print_osabi(unsigned char *e_ident)
 {
@@ -183,7 +177,7 @@ void print_osabi(unsigned char *e_ident)
 
 /**
  * print_abi - Prints the ABI version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF ABI version.
+ * @e_ident:  pointer to the  array containing the ABI version.
  */
 void print_abi(unsigned char *e_ident)
 {
@@ -193,8 +187,8 @@ void print_abi(unsigned char *e_ident)
 
 /**
  * print_type - Prints the type of an ELF header.
- * @e_type: The ELF type.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @e_type:  ELF type.
+ * @e_ident: pointer to the  array containing the class.
  */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
@@ -227,8 +221,8 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 
 /**
  * print_entry - Prints the entry point of an ELF header.
- * @e_entry: The address of the ELF entry point.
- * @e_ident: A pointer to an array containing the ELF class.
+ * @e_entry: ELF entry point.
+ * @e_ident:  pointer to the  array containing ELF class.
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
@@ -249,10 +243,10 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 }
 
 /**
- * close_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
+ * close_elf - Closes  ELF file.
+ * @elf: The file descriptor of  ELF file.
  *
- * Description: If the file cannot be closed - exit code 98.
+ * return : If the file cannot be closed - exit code 98.
  */
 void close_elf(int elf)
 {
@@ -272,9 +266,7 @@ void close_elf(int elf)
  *
  * Return: 0 on success.
  *
- * Description: If the file is not an ELF File or
- *              the function fails - exit code 98.
- */
+*/
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
@@ -307,7 +299,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
 	print_data(header->e_ident);
-	print_version(header->e_ident);
+	print_version(header->e_ident);  
 	print_osabi(header->e_ident);
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
@@ -317,4 +309,5 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	close_elf(o);
 	return (0);
 }
+
 
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,16 +23,16 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - a program that copies the content of a file to another file.
  * @argc: number of arguments.
  * @argv: arguments vector.
- * Return: Always 0.
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	ssize_t n, w;
+	char buffet[1024];
 
 	if (argc != 3)
 	{
@@ -44,14 +44,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	n = 1024;
+	while (n == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		n = read(file_from, buffer, 1024);
+		if (n == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		w = write(file_to, buffer, n);
+		if (w == -1)
 			error_file(0, -1, argv);
 	}
 
@@ -70,4 +70,5 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
 
