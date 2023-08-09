d1cc8cfdc8d96ded8aee0fac11bc051f4fa1cf1c
MAHA EL OUARDI <maha600019@gmail.com>
2023-08-09 06:23:38 -0700

    filres2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,36 +9,33 @@
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int f;
-	int i, j;
+	ssize_t r, w;
 	char *buffer;
 
 	if (!filename)
 		return (0);
-	f= open(filename, O_RDONLY);
-	if (f < 0)
+
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
+
+	buffer = malloc(sizeof(char) * (letters));
 	if (!buffer)
 		return (0);
-	i = read(f, buffer, letters);
-	if (i < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	buf[i] = '\0';
+
+	r = read(f, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
 	close(f);
-	j = write(STDOUT_FILENO, buffer, i);
-	if (j < 0)
-	{
-		free(buffer);
-		return (0);
-	}
+
 	free(buffer);
-	return (j);
+
+	return (w);
 }
 
 
