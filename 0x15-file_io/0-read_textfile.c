538931c8ac38cf581795f55935206f7ae6205853
MAHA EL OUARDI <maha600019@gmail.com>
2023-08-26 05:01:11 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile -ba function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile -ba function that reads a text file
+ * and prints it to the POSIX standard output.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
  *           function should read and print.
